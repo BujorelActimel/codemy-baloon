6560b9bc8fde3b76392b0b80838db3618bb1354e
bujor <bujormihaialexandru@gmail.com>
2025-01-27 17:51:46 +0200

    init

---
+++ baloon.py
@@ -0,0 +1,28 @@
+import pgzrun
+import os
+from random import randint
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = 'center'
+
+WIDTH = 800
+HEIGHT = 600
+
+# actors
+pass
+
+# globals
+pass
+
+def draw():
+    pass
+
+def on_mouse_down():
+    pass
+
+def on_mouse_up():
+    pass
+
+def update():
+    pass
+
+pgzrun.go()
